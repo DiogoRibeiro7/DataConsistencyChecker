c45fb6e399229ada4239d780ae5357a576b1c0b0
Diogo Ribeiro <diogo_dj@hotmail.com>
2026-09-23 09:01:07 +0100

    Export structured report model

--- src/data_consistency_checker/__init__.py
+++ src/data_consistency_checker/__init__.py
@@ -1,7 +1,8 @@
 """DataConsistencyChecker public package API."""
 
 from .checker import DataConsistencyChecker
+from .report import DataConsistencyReport
 
-__all__ = ["DataConsistencyChecker"]
+__all__ = ["DataConsistencyChecker", "DataConsistencyReport"]
 
 __version__ = "0.1.0"
